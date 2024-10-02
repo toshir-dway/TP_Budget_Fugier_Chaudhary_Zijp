0f9053b1c90a90599f6e3ff56c84d292dfe9654a
Corentin Fugier <corentinfugier@hotmail.com>
2024-10-02 18:48:59 +0200

    Update temporaire.py

--- temporaire.py
+++ temporaire.py
@@ -1,3 +1,8 @@
+import pandas as pd
+
+
+
+
 def add_expense(categories):
     category = input("Quelle est la categorie de cette depense ?")
     while category not in categories:
@@ -8,7 +13,7 @@ def add_expense(categories):
     value = input("Quelle est la valeur de cette depense ?")
     while True:
             try:
-                value = int(value)
+                value = -abs(int(value))
                 break  # Exit the loop if the conversion is successful
             except ValueError:
                 print("La valeur doit être un nombre entier.")
@@ -21,7 +26,7 @@ def test_add_expense():
     pass
 
 
-def accueil(categories):
+def accueil(categories, df):
     while True:  # Create a loop to allow repeated menu access
         print("Ajouter une dépense : 1")
         print("Ajouter un revenu : 2")
@@ -31,9 +36,14 @@ def accueil(categories):
         fonctionnalite = input("Choisissez une option: ")
 
         if fonctionnalite == "1":
-            # Fonction ajouter une dépense
-            print("Ajout d'une dépense...")
-            add_expense(categories)
+            category, description, value = add_expense(categories)
+            # Create a new row as a dictionary
+            new_row = pd.DataFrame({'Category': [category], 'Description': [description], 'Value': [value], 'Type': ['expense']})
+            # Append the new row to the DataFrame
+            df = pd.concat([df, new_row], ignore_index=True)
+            print("Dépense ajoutée avec succès.")
+            print(df)  # Display the updated DataFrame
+
         elif fonctionnalite == "2":
             # Fonction ajouter un revenu
             print("Ajout d'un revenu...")
@@ -51,8 +61,10 @@ def accueil(categories):
             break  # Exit the loop
         else:
             print("Option invalide, veuillez réessayer.")
+
+df=pd.DataFrame(data=None, columns=['Type', 'Category', 'Value', 'Description'])
 categories = ['Food', 'Transport', 'Utilities', 'Entertainment']
 
 
 # Call the accueil function to run the menu
-accueil(categories)
+accueil(categories, df)
